0ad4f315457f5f74df2be557b691a5fc22bcfada
XayviLG <javier_lealg@hotmail.com>
2023-07-14 18:11:54 +0200

    ticket 5

--- userdata.sh
+++ userdata.sh
@@ -1,22 +1,28 @@
-#! /bin/bash
+#!/bin/bash
 
-sudo yum update -y
-sudo yum install -y httpd php mysql mysql-server
-sudo service httpd start
-sudo chkconfig httpd on
-sudo chmod 777 /var/www
-sudo systemctl enable mariadb
-sudo systemctl start mariadb
-sudo chmod 777 /var/www/html
+echo -e "\n\nUpdating Apt Packages and upgrading latest patches\n"
+sudo apt-get update -y && sudo apt-get upgrade -y
 
-sudo mysql -e "CREATE DATABASE webserver /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-sudo mysql -e "CREATE USER Javier@localhost IDENTIFIED BY 'javier2468';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON mydb.* TO 'Javier'@'localhost';"
-sudo mysql -e "FLUSH PRIVILEGES;"
+echo -e "\n\nInstalling Apache2 Web server\n"
+sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
 
-curl -LO https://wordpress.org/latest.zip
-sudo mv latest.zip /var/www/html
-cd /var/www/html
-sudo unzip latest.zip
-sudo mv -f wordpress/* ./
-sudo service httpd restartyes
+echo -e "\n\nInstalling PHP & Requirements\n"
+sudo apt-get install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php-pear php7.0-mcrypt php7.0-mysql -y
+
+echo -e "\n\nInstalling MySQL\n"
+sudo apt-get install mysql-server mysql-client -y
+
+echo -e "\n\nPermissions for /var/www\n"
+sudo chown -R www-data:www-data /var/www
+echo -e "\n\n Permissions have been set\n"
+
+echo -e "\n\nEnabling Modules\n"
+sudo a2enmod rewrite
+sudo phpenmod mcrypt
+
+echo -e "\n\nRestarting Apache\n"
+sudo service apache2 restart
+
+echo -e "\n\nLAMP Installation Completed"
+
+exit 0
