d7e57513bf38b69419a27654ca255acd58f3c1a6
XayviLG <javier_lealg@hotmail.com>
2023-07-22 09:10:56 +0200

    Autoscaling and ELB

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe # xtrace and errexit 
+#!/bin/bash -xe
 # STEP 1 - Setpassword & DB Variables
 
 DBName='example'
@@ -13,10 +13,10 @@ sudo yum install -y wget php-mysqlnd httpd php-fpm php-mysqli mariadb105-server 
 # STEP 3 - Web and DB Servers Online - and set to startup
 
 
-systemctl enable httpd #manages both system and service configurations, enabling administrators to manage the OS and control the status of services
-systemctl enable mariadb #""
-systemctl start httpd #""
-systemctl start mariadb #""
+systemctl enable httpd 
+systemctl enable mariadb 
+systemctl start httpd 
+systemctl start mariadb 
 
 # STEP 4 - Set Mariadb Root Password
 
@@ -26,31 +26,31 @@ mysqladmin -u root password $DBRootPassword
 
 wget http://wordpress.org/latest.tar.gz -P /var/www/html
 cd /var/www/html
-tar -zxvf latest.tar.gz #tar it’s used for creating & extracting archive files/zxvf (compress,extracts,verb.,filename)
-cp -rvf wordpress/* . #copy (force copy by removing ,verb.,  )
-rm -R wordpress #remove
-rm latest.tar.gz #remove
+tar -zxvf latest.tar.gz 
+cp -rvf wordpress/* . 
+rm -R wordpress 
+rm latest.tar.gz
 
 # STEP 6 - Configure Wordpress
 
 cp ./wp-config-sample.php ./wp-config.php
-sed -i "s/'database_name_here'/'$DBName'/g" wp-config.php #(Stream editor) sed -i 's/old-text/new-text/g' input.txt.
-sed -i "s/'username_here'/'$DBUser'/g" wp-config.php #""
-sed -i "s/'password_here'/'$DBPassword'/g" wp-config.php #""
+sed -i "s/'database_name_here'/'$DBName'/g" wp-config.php 
+sed -i "s/'username_here'/'$DBUser'/g" wp-config.php 
+sed -i "s/'password_here'/'$DBPassword'/g" wp-config.php 
 
 # Step 6a - permissions
 
-usermod -a -G apache ec2-user #change the properties of a user in Linux through the command line
-chown -R ec2-user:apache /var/www # The chown command allows you to change the user and/or group ownership of a given file, directory, or symbolic link.
-chmod 2775 /var/www #used to change the access permissions of files and directories / First, it means that people in your group can create new files in that directory, but other people cannot
-find /var/www -type d -exec chmod 2775 {} \; #find / ""
-find /var/www -type f -exec chmod 0664 {} \; #find / Chmod 0664 sets permissions so that, (U)ser / owner can read, can write and can't execute. (G)roup can read, can write and can't execute. (O)thers can read, can't write and can't execute.
+usermod -a -G apache ec2-user 
+chown -R ec2-user:apache /var/www 
+chmod 2775 /var/www 
+find /var/www -type d -exec chmod 2775 {} \; 
+find /var/www -type f -exec chmod 0664 {} \; 
 
 # STEP 7 Create Wordpress DB
 
-echo "CREATE DATABASE $DBName;" >> /tmp/db.setup #echo is used to display line of text/string that are passed as an argument
-echo "CREATE USER '$DBUser'@'localhost' IDENTIFIED BY '$DBPassword';" >> /tmp/db.setup #""
-echo "GRANT ALL ON $DBName.* TO '$DBUser'@'localhost';" >> /tmp/db.setup #""
-echo "FLUSH PRIVILEGES;" >> /tmp/db.setup #""
+echo "CREATE DATABASE $DBName;" >> /tmp/db.setup 
+echo "CREATE USER '$DBUser'@'localhost' IDENTIFIED BY '$DBPassword';" >> /tmp/db.setup 
+echo "GRANT ALL ON $DBName.* TO '$DBUser'@'localhost';" >> /tmp/db.setup 
+echo "FLUSH PRIVILEGES;" >> /tmp/db.setup 
 mysql -u root --password=$DBRootPassword < /tmp/db.setup
-sudo rm /tmp/db.setup #sudo -allows you to run programs with the security privileges of another user 
+sudo rm /tmp/db.setup
