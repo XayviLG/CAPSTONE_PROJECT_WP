c1661d0fe2aaceba630f24ba634b8f50957fb376
XayviLG <javier_lealg@hotmail.com>
2023-07-22 09:12:15 +0200

    C2. App load balancer, listener , target group

---
+++ elb.tf
@@ -0,0 +1,32 @@
+#Application load balancer
+
+resource "aws_lb" "my_alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.devVPC_sg_allow_http.id]
+  subnets            = [aws_subnet.devVPC_public_subneta.id, aws_subnet.devVPC_public_subnetb.id]
+}
+
+#Listener for Load Balancer
+
+resource "aws_lb_listener" "my_lb_listener" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_tg.arn
+  }
+}
+
+#Target group
+
+resource "aws_lb_target_group" "my_tg" {
+  name     = "my-tg"
+  target_type = "instance"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.devVPC.id
+}
