8a494548fd74665f2142ccc123c0292c9d495143
XayviLG <javier_lealg@hotmail.com>
2023-07-27 22:07:58 +0200

    SCRUM adding s3 for storing wordpress

---
+++ s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "mys3-wordpress" {
+  bucket = "mys3-wordpress"
+  
+  tags = {
+    Name = "myS3-wordpress"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3-ownership" {
+  bucket = aws_s3_bucket.mys3-wordpress.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "mys3-wordpress-acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3-ownership]
+  bucket = aws_s3_bucket.mys3-wordpress.id
+  acl    = "private"
+}
