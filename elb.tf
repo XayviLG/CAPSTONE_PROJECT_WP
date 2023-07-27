e5c11539a13c912460638a772033955be6e721e1
XayviLG <javier_lealg@hotmail.com>
2023-07-27 22:09:56 +0200

    C2 ALB, Target and Listener / Scrum

--- elb.tf
+++ elb.tf
@@ -1,32 +1,34 @@
-#Application load balancer
+#Load Balancer
 
-resource "aws_lb" "my_alb" {
-  name               = "my-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.devVPC_sg_allow_http.id]
-  subnets            = [aws_subnet.devVPC_public_subneta.id, aws_subnet.devVPC_public_subnetb.id]
-}
-
-#Listener for Load Balancer
-
-resource "aws_lb_listener" "my_lb_listener" {
-  load_balancer_arn = aws_lb.my_alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.my_tg.arn
+resource "aws_lb" "my_loadBalancer" {
+  name                              = "myWebserver-alb"
+  internal                          = false
+  load_balancer_type                = "application"
+  security_groups                   = [aws_security_group.my-alb-sg.id]
+  subnets                           = [aws_subnet.my_publicSubnet_1.id, aws_subnet.my_publicSubnet_2.id]
+  enable_deletion_protection        = false
+    tags = {
+        Environment                 = "production"
   }
 }
 
-#Target group
+#Target Group
 
-resource "aws_lb_target_group" "my_tg" {
-  name     = "my-tg"
-  target_type = "instance"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.devVPC.id
+resource "aws_lb_target_group" "my-target-group" {
+  name                              = "CPUtest-tg"
+  port                              = 80
+  protocol                          = "HTTP"
+  vpc_id                            = aws_vpc.myVPC.id
+}
+
+#Listener
+
+resource "aws_lb_listener" "my-listener" {
+  load_balancer_arn                 = aws_lb.my_loadBalancer.arn
+  port                              = 80
+  protocol                          = "HTTP"
+  default_action {
+    type                            = "forward"
+    target_group_arn                = aws_lb_target_group.my_target-group.arn
+  }
 }
