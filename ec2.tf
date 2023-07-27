26f9fe6cbc563a64ade705b1092099120cce0981
XayviLG <javier_lealg@hotmail.com>
2023-07-27 22:12:55 +0200

    C2 SCRUM Bastion

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,33 @@
-resource "aws_instance" "wordpress"{
-    ami = "ami-0507f77897697c4ba"
-    instance_type = "t2.micro"
-    key_name = "cool"
-    vpc_security_group_ids = [aws_security_group.devVPC_sg_allow_http.id]
-    subnet_id = aws_subnet.devVPC_public_subneta.id
-    user_data = "${file("userdata.sh")}"
-    tags = {
-        Name = "wordpress"
-    }
-    provisioner "local-exec"{
-    command = "echo Instance Type=${self.instance_type},Instance ID=${self.id},Public DNS=${self.public_dns},AMI ID=${self.ami} >> allinstancedetails"
+# Select newest AMI-id
+
+data "aws_ami" "my_latest_linux_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["amazon"] # Amazon
 }
-#
+
+###Create EC2
+resource "aws_instance" "my_Bastion"{
+    #ami                    = data.aws_ami.nf_latest_linux_ami.id
+    ami = "ami-08541bb85074a743a"
+    instance_type          = "t2.micro"
+    key_name               = "vockey"
+    vpc_security_group_ids = [aws_security_group.my_sg_bastion.id]
+    subnet_id              = aws_subnet.my_publicSubnet_1.id
+    tags = {
+        Name = "my_Bastion"
+        }
+    
+    provisioner "local-exec"{
+        command = "echo Instance Type=${self.instance_type},Instance ID=${self.id},Public DNS=${self.public_dns},AMI ID=${self.ami} >> allinstancedetails"
+    }
+}
