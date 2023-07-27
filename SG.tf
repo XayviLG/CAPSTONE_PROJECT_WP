7b44c9c3a129ea859e8f3a429def59dc80556b0f
XayviLG <javier_lealg@hotmail.com>
2023-07-27 22:07:18 +0200

    C1 new SG Bastion etc. SCRUM

--- SG.tf
+++ SG.tf
@@ -1,43 +1,92 @@
-resource "aws_security_group" "devVPC_sg_allow_http"{
-    vpc_id = aws_vpc.devVPC.id
-    name = "devVPC_terraform_vpc_allow_http"
+# Security Group for Bastion
+resource "aws_security_group" "my_sg_bastion"{
+    vpc_id = aws_vpc.myVPC.id
+    name = "my_securityGrouplatest"
     tags = {
-        Name = "devVPC_terraform_sg_allow_http"
-    }
+        Name = "cap-sg-bastion"
+    }     
 }
-# SHH Port 22 Ingress Security Port 22 (Inbound) - Provides a security group rule resource (https://registry.terraform.io.providers/hashicorp/aws/latest/docs/resources/security_group_rule)
-resource "aws_security_group_rule" "devVPC_ssh_ingress_access"{
+
+# Rules bastion - Ingress Security Port 22 (Inbound)
+resource "aws_security_group_rule" "my_ingress_ssh"{
     from_port = 22
     protocol = "tcp"
-    security_group_id = aws_security_group.devVPC_sg_allow_http.id
-    to_port = 22
+    security_group_id = aws_security_group.my_sg_bastion.id
+    to_port= 22
     type = "ingress"
-    cidr_blocks = [var.cidr_blocks]
+    cidr_blocks = ["0.0.0.0/0"] # <Specify the CIDR> instead of allowing it to public
 }
-# HTTP Ingress Security Port 80 (Inbound)
-resource "aws_security_group_rule" "devVPC_http_ingress_access"{
-    from_port = 80
+
+# Rules bastion - Allow Access All (Outbound)
+resource "aws_security_group_rule" "my_outbound_all"{
+    from_port = 22
     protocol = "tcp"
-    security_group_id = aws_security_group.devVPC_sg_allow_http.id
-    to_port= 80
-    type = "ingress"
-    cidr_blocks = [var.cidr_blocks]
-}
-# HTTPS Ingress Security Port 8080 or 443 (Inbound) Port 80 is the default port for unencrypted HTTP traffic, while Port 8080 is often used as an alternative HTTP port for web servers and applications. Port 443 is used as the default port for encrypted HTTPS traffic, which is essential for transmitting sensitive information securely.
-resource "aws_security_group_rule" "devVPC_http8080_ingress_access"{
-    from_port = 443
-    protocol = "tcp"
-    security_group_id = aws_security_group.devVPC_sg_allow_http.id
-    to_port= 443
-    type = "ingress"
-    cidr_blocks = [var.cidr_blocks]
-}
-# Egress Security (Outbound) , Allow all outbound traffic
-resource "aws_security_group_rule" "devVPC_egress_access" {
-    from_port   = 0
-    protocol    = "-1"
-    security_group_id = aws_security_group.devVPC_sg_allow_http.id
-    to_port     = 0
+    security_group_id = aws_security_group.my_sg_bastion.id
+    to_port= 22
     type = "egress"
-    cidr_blocks = [var.cidr_blocks]
+    cidr_blocks = ["0.0.0.0/0"]
 }
+# Security Group for Application Load balancer and rules
+resource "aws_security_group" "my-alb-sg"{
+        vpc_id                      = aws_vpc.myVPC.id
+        name                        = "alb-sg"
+        tags = {
+            Name = "cap-alb-sg"
+        }
+    }
+    resource "aws_security_group_rule" "alb-sg-http-in"{
+        from_port                   = 80
+        protocol                    = "tcp"
+        security_group_id           = aws_security_group.my-alb-sg.id
+        to_port                     = 80
+        type                        = "ingress"
+        cidr_blocks                 = ["0.0.0.0/0"]
+    }
+    resource "aws_security_group_rule" "alb-sg-https-in"{
+        from_port                   = 443
+        protocol                    = "tcp"
+        security_group_id           = aws_security_group.my-alb-sg.id
+        to_port                     = 443
+        type                        = "ingress"
+        cidr_blocks                 = ["0.0.0.0/0"]
+    }
+    resource "aws_security_group_rule" "alb-sg-all-out"{
+        from_port                   = 0
+        protocol                    = "all"
+        security_group_id           = aws_security_group.my-alb-sg.id
+        to_port                     = 65535
+        type                        = "egress"
+        cidr_blocks                 = ["0.0.0.0/0"]
+    }
+#Security Group for Autoscaling and rules
+resource "aws_security_group" "my-autoscaling-sg"{
+        vpc_id                      = aws_vpc.myVPC.id
+        name                        = "autoscaling-sg"
+        tags = {
+            Name = "cap-autoscaling-sg"
+        }
+    }
+    resource "aws_security_group_rule" "autoscaling-sg-ssh-in"{
+        from_port                   = 22
+        protocol                    = "tcp"
+        security_group_id           = aws_security_group.my-autoscaling-sg.id
+        to_port                     = 22
+        type                        = "ingress"
+        cidr_blocks                 = ["0.0.0.0/0"]
+    }
+    resource "aws_security_group_rule" "autoscaling-sg-http-in"{
+        from_port                   = 80
+        protocol                    = "tcp"
+        security_group_id           = aws_security_group.my-autoscaling-sg.id
+        to_port                     = 80
+        type                        = "ingress"
+        cidr_blocks                 = ["0.0.0.0/0"]
+    }
+    resource "aws_security_group_rule" "autoscaling-sg-all-out"{
+        from_port                   = 0
+        protocol                    = "all"
+        security_group_id           = aws_security_group.my-autoscaling-sg.id
+        to_port                     = 65535
+        type                        = "egress"
+        cidr_blocks                 = ["0.0.0.0/0"]
+    }
