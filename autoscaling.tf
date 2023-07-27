0c7445ec1100250840b8ae855fed61a09ea320d0
XayviLG <javier_lealg@hotmail.com>
2023-07-27 22:11:25 +0200

    C2 Launch template Autoscaling Group policy/SCRUM

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,111 +1,47 @@
-#Creating an AMI for Autoscaling
-
-resource "aws_ami_from_instance" "autoami" {
-  name               = "auto_scaling_ami"
-  source_instance_id = aws_instance.wordpress.id
-}
-
 #Launch Template
 
-resource "aws_launch_template" "my_launch_template" {
-
-  name = "my_launch_template"
-  
-  image_id = "ami-0507f77897697c4ba"
-  instance_type = "t2.micro"
-  key_name = "cool"
-  user_data = filebase64("${path.module}/userdata.sh")
-
-  block_device_mappings {
-    device_name = "/dev/sda1"
-
-    ebs {
-      volume_size = 8
-      volume_type = "gp2"
-    }
-  }
-
-  network_interfaces {
-    associate_public_ip_address = true
-    security_groups = [aws_security_group.devVPC_sg_allow_http.id]
+resource "aws_launch_template" "my-launch-template" {
+  name                              = "WebserverLaunchTemplate"
+  #image_id                         = data.aws_ami.amzLinux.id
+  image_id                          = "ami-08541bb85074a743a"
+  instance_type                     = "t2.micro"
+  vpc_security_group_ids            = [aws_security_group.my-autoscaling-sg.id]
+  user_data                         = filebase64("CPUstresstest.sh")
+  tag_specifications {
+        resource_type = "instance"
+        tags          = {
+            Name      = "cap_autoserver"
+   }
   }
 }
 
 #Autoscaling Group
 
-resource "aws_autoscaling_group" "my_asg" {
-  name                      = "my_asg"
-  max_size                  = 4
-  min_size                  = 2
-  health_check_type         = "ELB"    # optional
-  health_check_grace_period = 300
-  desired_capacity          = 2
-  target_group_arns = [aws_lb_target_group.my_tg.arn]
-
-  vpc_zone_identifier       = [aws_subnet.private_subneta.id, aws_subnet.private_subnetb.id]
-  
+resource "aws_autoscaling_group" "my_AutoScalingGroup" {
+  name                              = "my-autoscaling-group"
+  max_size                          = 4
+  min_size                          = 2
+  desired_capacity                  = 2
+  vpc_zone_identifier               = [aws_subnet.my_privateSubnet_1.id, aws_subnet.my_privateSubnet_2.id]
+  target_group_arns                 = [aws_lb_target_group.target-group.arn]
+  health_check_type                 = "ELB"
+  health_check_grace_period         = 300
   launch_template {
-    id      = aws_launch_template.my_launch_template.id
-    version = "$Latest"
+    id                = aws_launch_template.my-launch-template.id
+    version           = "$Latest"
   }
 }
 
-#Scale-up policy
+#Autoscaling policy
 
-resource "aws_autoscaling_policy" "scale_up" {
-  name                   = "scale_up"
-  policy_type            = "SimpleScaling"
-  autoscaling_group_name = aws_autoscaling_group.my_asg.name
-  adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = "1"      # add one instance
-  cooldown               = "120"    # cooldown period after scaling
-}
-
-#Scale-up cloud-watch alarm
-
-resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
-  alarm_name          = "scale-up-alarm"
-  alarm_description   = "asg-scale-up-cpu-alarm"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = "50"
-  dimensions = {
-    "AutoScalingGroupName" = aws_autoscaling_group.my_asg.name
+resource "aws_autoscaling_policy" "policy" {
+  name                              = "CPUpolicy"
+  policy_type                       = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type        = "ASGAverageCPUUtilization"
+    }
+      target_value                  = 75.0
   }
-  actions_enabled = true
-  alarm_actions   = [aws_autoscaling_policy.scale_up.arn]
-}
-
-#Scale-down policy
-
-resource "aws_autoscaling_policy" "scale_down" {
-  name                   = "asg-scale-down"
-  autoscaling_group_name = aws_autoscaling_group.my_asg.name
-  adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = "-1"
-  cooldown               = "300"
-  policy_type            = "SimpleScaling"
-}
-
-#Scale-down cloud-watch alarm
-
-resource "aws_cloudwatch_metric_alarm" "scale_down_alarm" {
-  alarm_name          = "asg-scale-down-alarm"
-  alarm_description   = "asg-scale-down-cpu-alarm"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = "30"
-  dimensions = {
-    "AutoScalingGroupName" = aws_autoscaling_group.my_asg.name
-  }
-  actions_enabled = true
-  alarm_actions   = [aws_autoscaling_policy.scale_down.arn]
+  autoscaling_group_name            = aws_autoscaling_group.my_AutoScalingGroup.name
 }
