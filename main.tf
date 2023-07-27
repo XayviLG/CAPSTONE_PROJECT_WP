10cedf214d950e34c38185cceb28337b43476c14
XayviLG <javier_lealg@hotmail.com>
2023-07-27 22:08:41 +0200

    C1 providers

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
+provider "aws" {
+  region = "us-west-2"
+}
 terraform {
     required_providers {
         aws = {
             source  = "hashicorp/aws"
-            version = "5.8.0"
+            version = "~> 5.8.0"
         }
     }
 }
-provider "aws"{
-    region = var.AWS_REGION
-}
-
-#
