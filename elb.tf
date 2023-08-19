150d2baabf49858bc03234166ab86e9d9d8a8d55
XayviLG <javier_lealg@hotmail.com>
2023-08-19 09:59:25 +0200

    ALB, Target group, Listener

--- elb.tf
+++ elb.tf
@@ -1,11 +1,11 @@
 #Load Balancer
 
-resource "aws_lb" "my_loadBalancer" {
+resource "aws_lb" "cpstn_loadBalancer" {
   name                              = "myWebserver-alb"
   internal                          = false
   load_balancer_type                = "application"
-  security_groups                   = [aws_security_group.my-alb-sg.id]
-  subnets                           = [aws_subnet.my_publicSubnet_1.id, aws_subnet.my_publicSubnet_2.id]
+  security_groups                   = [aws_security_group.cpstn_alb_sg.id]
+  subnets                           = [aws_subnet.cpstn_publicsubnet_1.id, aws_subnet.cpstn_publicsubnet_2.id]
   enable_deletion_protection        = false
     tags = {
         Environment                 = "production"
@@ -14,21 +14,21 @@ resource "aws_lb" "my_loadBalancer" {
 
 #Target Group
 
-resource "aws_lb_target_group" "my-target-group" {
+resource "aws_lb_target_group" "cpstn-target-group" {
   name                              = "CPUtest-tg"
   port                              = 80
   protocol                          = "HTTP"
-  vpc_id                            = aws_vpc.myVPC.id
+  vpc_id                            = aws_vpc.cpstn_vpc.id
 }
 
 #Listener
 
-resource "aws_lb_listener" "my-listener" {
-  load_balancer_arn                 = aws_lb.my_loadBalancer.arn
+resource "aws_lb_listener" "cpstn-listener" {
+  load_balancer_arn                 = aws_lb.cpstn_loadBalancer.arn
   port                              = 80
   protocol                          = "HTTP"
   default_action {
     type                            = "forward"
-    target_group_arn                = aws_lb_target_group.my-target-group.arn
+    target_group_arn                = aws_lb_target_group.cpstn-target-group.arn
   }
 }
