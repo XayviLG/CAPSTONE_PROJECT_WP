e68113177f567a726ce9555457c0f01afec5931d
XayviLG <javier_lealg@hotmail.com>
2023-08-19 10:02:07 +0200

    Vars CIDR block

--- vars.tf
+++ vars.tf
@@ -1,24 +1,22 @@
-/**variable "AWS_REGION"{
-    default="us-west-2"
-    description="AWS Region"
+variable "AWS_REGION"{
+    default           = "us-west-2"
+    description       = "AWS Region"
 }
-
 variable "cidr_blocks"{
-    default = "0.0.0.0/0"
+    default           = "0.0.0.0/0"
 }
-
-variable "cidr_block_dev_vpc" {
-    default = "10.0.0.0/16"
+variable "cidr_block_cpstn_vpc" {
+    default           = "10.0.0.0/16"
 }
-variable "cidr_block_public_subneta"{
-    default = "10.0.0.0/24"
+variable "cidr_block_cpstn_publicsubnet_1"{
+    default           = "10.0.1.0/24"
 }
-variable "cidr_block_private_subneta"{
-    default = "10.0.1.0/24"
+variable "cidr_block_cpstn_privatesubnet_1"{
+    default           = "10.0.2.0/24"
 }
-variable "cidr_block_public_subnetb"{
-    default = "10.0.2.0/24"
+variable "cidr_block_cpstn_publicsubnet_2"{
+    default           = "10.0.3.0/24"
 }
-variable "cidr_block_private_subnetb"{
-    default = "10.0.3.0/24"
-}**/
+variable "cidr_block_cpstn_privatesubnet_2"{
+    default           = "10.0.4.0/24"
+}
