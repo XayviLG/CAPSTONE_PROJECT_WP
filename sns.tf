16e14614ddc715864a8b9faf22977e00af4f0a1a
XayviLG <javier_lealg@hotmail.com>
2023-08-19 10:07:46 +0200

    Adding SNS notifications

--- sns.tf
+++ sns.tf
@@ -1,20 +1,20 @@
 resource "aws_autoscaling_notification" "autoscaling_notifications" {
-  group_names = [
-    aws_autoscaling_group.my_AutoScalingGroup.name
+    group_names      = [
+    aws_autoscaling_group.cpstn_AutoScalingGroup.name
   ]
-  notifications = [
+  notifications      = [
     "autoscaling:EC2_INSTANCE_LAUNCH",
     "autoscaling:EC2_INSTANCE_TERMINATE",
     "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
     "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
   ]
-  topic_arn = aws_sns_topic.autoscaling_updates.arn
+    topic_arn        = aws_sns_topic.autoscaling_updates.arn
 }
 resource "aws_sns_topic" "autoscaling_updates" {
-  name = "autoscaling-updates"
+    name             = "autoscaling-updates"
 }
-resource "aws_sns_topic_subscription" "nf-sns-subscription" {
-  topic_arn = aws_sns_topic.autoscaling_updates.arn
-  protocol  = "email"
-  endpoint  = "ichbinjavi@hotmail.com"
+resource "aws_sns_topic_subscription" "cpstn-sns-subscription" {
+    topic_arn        = aws_sns_topic.autoscaling_updates.arn
+    protocol         = "email"
+    endpoint         = "ichbinjavi@hotmail.com"
 }
