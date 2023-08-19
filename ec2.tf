609d734421ab5bbad4277674a3a95937d32566ac
XayviLG <javier_lealg@hotmail.com>
2023-08-19 09:56:54 +0200

    New AMI resource for Bastion host

--- ec2.tf
+++ ec2.tf
@@ -1,33 +1,28 @@
 # Select newest AMI-id
 
-data "aws_ami" "my_latest_linux_ami" {
-  most_recent = true
+data "aws_ami" "cpstn_latest_linux_ami" {
+  most_recent              = true
+  owners                   = ["amazon"] # Amazon
 
   filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    name                   = "name"
+    values                 = ["al2023-ami-2023*x86_64"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["amazon"] # Amazon
 }
 
 ###Create EC2
-resource "aws_instance" "my_Bastion"{
-    ami                    = data.aws_ami.my_latest_linux_ami.id
+resource "aws_instance" "cpstn_Bastion"{
+    ami                    = data.aws_ami.cpstn_latest_linux_ami.id
    #ami                    = "ami-08541bb85074a743a"
     instance_type          = "t2.micro"
     key_name               = "vockey"
-    vpc_security_group_ids = [aws_security_group.my_sg_bastion.id]
-    subnet_id              = aws_subnet.my_publicSubnet_1.id
+    vpc_security_group_ids = [aws_security_group.cpstn_sg_bastion.id]
+    subnet_id              = aws_subnet.cpstn_publicsubnet_1.id
     tags = {
-        Name = "my_Bastion"
+        Name               = "cpstn_Bastion"
         }
     
     provisioner "local-exec"{
-        command = "echo Instance Type=${self.instance_type},Instance ID=${self.id},Public DNS=${self.public_dns},AMI ID=${self.ami} >> allinstancedetails"
+        command            = "echo Instance Type=${self.instance_type},Instance ID=${self.id},Public DNS=${self.public_dns},AMI ID=${self.ami} >> metadata"
     }
 }
