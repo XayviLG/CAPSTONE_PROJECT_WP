63474e910e6e036e1b43e102bbad092787d25229
XayviLG <javier_lealg@hotmail.com>
2023-08-19 09:49:53 +0200

    Get Wordpress files from S3, adding IAM profile

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,62 +1,62 @@
 data "aws_ami" "amzLinux" {
-        most_recent = true
-        owners      = ["amazon"]
+        most_recent                 = true
+        owners                      = ["amazon"]
     
     filter {
-        name        = "name"
-        values      = ["al2023-ami-2023*x86_64"]
+        name                        = "name"
+        values                      = ["al2023-ami-2023*x86_64"]
         }
 }
  locals {
-        DB          = "mydb"
-        User        = "Admin"
-        PW          = "password123"
-        host        = aws_db_instance.cpstn-sql-db.address
+        DB   = "mydb"
+        User = "Admin"
+        PW   = "password123"
+        host = aws_db_instance.cpstn-sql-db.address
 }
 
 #Launch Template
 
-resource "aws_launch_template" "my-launch-template" {
-  name              = "WebserverLaunchTemplate"
-  image_id          = data.aws_ami.amzLinux.id
-  #image_id         = "ami-08541bb85074a743a"
-  instance_type     = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.my-autoscaling-sg.id]
-  user_data              = base64encode(templatefile("newuserdata.sh",{
-        DB          = local.DB
-        User        = local.User
-        PW          = local.PW
-        host        = local.host
+resource "aws_launch_template" "cpstn-launch-template" {
+  name                              = "WebserverLaunchTemplate"
+  image_id                          = data.aws_ami.amzLinux.id
+  #image_id                         = "ami-08541bb85074a743a"
+  instance_type                     = "t2.micro"
+  vpc_security_group_ids            = [aws_security_group.cpstn_autoscaling_sg.id]
+  user_data                         = base64encode(templatefile("newuserdata.sh",{
+        DB   = local.DB
+        User = local.User
+        PW   = local.PW
+        host = local.host
     })) 
 
-  #IAM profile for Labenvironment
+  #IAM profile for Lab environment
 
   iam_instance_profile {
-        name          =  "LabInstanceProfile"
+        name                        =  "LabInstanceProfile"
    }     
   tag_specifications {
-        resource_type = "instance"
-        tags          = {
-            Name      = "mysqlserver"
+        resource_type               = "instance"
+        tags                        = {
+            Name                    = "mysqlserver"
    }
   }
 }
 
 #Autoscaling Group
 
-resource "aws_autoscaling_group" "my_AutoScalingGroup" {
-  name                      = "my-autoscaling-group"
-  max_size                  = 4
-  min_size                  = 2
-  desired_capacity          = 2
+resource "aws_autoscaling_group" "cpstn_AutoScalingGroup" {
+  name                              = "cpstn-autoscaling-group"
+  max_size                          = 4
+  min_size                          = 2
+  desired_capacity                  = 2
   #changed to public subnets while NATGateway is turned off 
-  vpc_zone_identifier       = [aws_subnet.my_publicSubnet_1.id, aws_subnet.my_publicSubnet_2.id]
-  target_group_arns         = [aws_lb_target_group.my-target-group.arn]
-  health_check_type         = "ELB"
-  health_check_grace_period = 300
+  vpc_zone_identifier               = [aws_subnet.cpstn_publicsubnet_1.id, aws_subnet.cpstn_publicsubnet_2.id]
+  target_group_arns                 = [aws_lb_target_group.cpstn-target-group.arn]
+  health_check_type                 = "ELB"
+  health_check_grace_period         = 300
   launch_template {
-    id                      = aws_launch_template.my-launch-template.id
-    version                 = "$Latest"
+    id                              = aws_launch_template.cpstn-launch-template.id
+    version                         = "$Latest"
   }
 }
 
@@ -71,5 +71,5 @@ resource "aws_autoscaling_policy" "policy" {
     }
       target_value                  = 75.0
   }
-  autoscaling_group_name            = aws_autoscaling_group.my_AutoScalingGroup.name
+  autoscaling_group_name            = aws_autoscaling_group.cpstn_AutoScalingGroup.name
 }
